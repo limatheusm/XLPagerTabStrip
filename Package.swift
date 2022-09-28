df08e4e50ed11190771d8fbd1684d5a851772901
limatheusm <limatheusm@gmail.com>
2022-09-28 10:18:51 -0300

    config: add spm support

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "XLPagerTabStrip",
+    platforms: [.iOS(.v11),],
+    products: [
+        .library(name: "XLPagerTabStrip", targets: ["XLPagerTabStrip"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "FXPageControl", path: "FXPageControl.xcframework"),
+        .target(
+            name: "XLPagerTabStrip",
+            dependencies: ["FXPageControl"],
+            exclude: ["Info.plist"],
+            resources: [.process("Resources")],
+            publicHeadersPath: "XLPagerTabStrip"
+        ),
+        .testTarget(
+            name: "XLPagerTabStripTests",
+            dependencies: ["XLPagerTabStrip"],
+            exclude: ["Info.plist"]
+        ),
+    ]
+)
